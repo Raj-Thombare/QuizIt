1a05a9f043b00c79e30ee1098464b7ef53a91ddd
Rakaa <therajthombare@gmail.com>
2022-12-26 03:23:53 +0530

    feat: create Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Home from "./components/Home";
+import Result from "./components/Result";
+import Quiz from "./components/Quiz";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Rakaa</div>;
+  return (
+    <div>
+      <div className="bgImage"></div>
+      <div className="App">
+        <Home />
+        <Result />
+        <Quiz />
+      </div>
+    </div>
+  );
 }
 
 export default App;

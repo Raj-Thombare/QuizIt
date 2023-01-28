bc3b5ea77f3887f1367741b12edc9b7113e3381a
Raj Thombare <therajthombare@gmail.com>
2023-01-28 23:08:53 +0530

    Home page done

--- src/context/data-context.js
+++ src/context/data-context.js
@@ -15,7 +15,6 @@ export const DataContextProvider = ({ children }) => {
         difficulty && `difficulty=${difficulty}`
       }`
     );
-    console.log(apiData.data);
     setQuestions(apiData);
   };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,84 @@
-import Form from "../components/Form/Form";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextField, MenuItem, Button } from "@mui/material";
+import Error from "../components/Error/Error";
+import Categories from "../adapters/category";
+import DataContext from "../context/data-context";
 
-import "./Home.css";
+import classes from "./Home.module.css";
 
-const Home = ({ name, setName, fetchQuestions, setShowProfile }) => {
+const Home = () => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { fetchQuestions, name, setName, setShowProfile } =
+    useContext(DataContext);
+
+  const handleSubmit = () => {
+    if (!name || !category || !difficulty) {
+      setError(true);
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+    }
+    navigate("/quiz");
+    setShowProfile(true);
+  };
+
   return (
-    <div className="content">
-      <Form
-        name={name}
-        setName={setName}
-        fetchQuestions={fetchQuestions}
-        setShowProfile={setShowProfile}
-      />
+    <div className={classes.content}>
+      <div className={classes.settings}>
+        <h1 style={{ padding: "5px 20px", color: "white" }}>
+          Welcome to QuizIt
+        </h1>
+        {error && <Error>Please fill all the fields!</Error>}
+        <div className={classes.settings__select}>
+          <TextField
+            label="Enter your name"
+            variant="outlined"
+            style={{ marginBottom: 25 }}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            select
+            label="Select Category"
+            value={category}
+            varient="outlined"
+            style={{ marginBottom: 30 }}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {Categories.map((option) => (
+              <MenuItem key={option.category} value={option.value}>
+                {option.category}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            select
+            label="Select Difficulty"
+            value={difficulty}
+            varient="outlined"
+            style={{ marginBottom: 30 }}
+            onChange={(e) => setDifficulty(e.target.value)}
+          >
+            <MenuItem key="easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Start Quiz
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };

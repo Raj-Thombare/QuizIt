5e3f46e55ceb7dc4af3f1406d3cb9bbf8bcbbd64
Raj Thombare <therajthombare@gmail.com>
2023-01-31 21:44:44 +0530

    auth done

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useData } from "../../context/data-context";
-import Navbar from "../../components/Navbar/Navbar";
 
 import classes from "./Result.module.css";
 
@@ -8,12 +7,9 @@ const Result = () => {
   const { score } = useData();
 
   return (
-    <>
-      <Navbar />
-      <div className={classes.result}>
-        <h2 className={classes.title}>Your score is {score}</h2>
-      </div>
-    </>
+    <div className={classes.result}>
+      <h2 className={classes.title}>Your score is {score}</h2>
+    </div>
   );
 };
 
